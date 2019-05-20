f0f47ecefb97312789ca4fded58dc00054808c6e
Lachlan Becirovic <101626407@student.swin.edu.au>
2019-05-20 11:22:08 +1000

    add pet files

--- NewPetFiles/New_Pet.php
+++ NewPetFiles/New_Pet.php
@@ -10,7 +10,7 @@ if (!$conn) {
 	die(json_encode(["isError" => true, "message" => "Connection failed: " . mysqli_connect_error()]));
 }
 
-$sql = "INSERT INTO `contact`(
+$sql = "INSERT INTO `pets`(
 	`Species`,
 	`Breed`,
 	`DOB`,
