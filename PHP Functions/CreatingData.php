2b102ffa753d206697c6dadda1e66d2bccdb5ff7
David Kevork <davidkevork@hotmail.com>
2019-05-29 00:49:36 +1000

    Fix CreateData for unit test

--- PHP Functions/CreatingData.php
+++ PHP Functions/CreatingData.php
@@ -46,29 +46,29 @@ class CreatingData extends Mysql
 		];
 
 		if (!in_array($_POST['State'], $State)) {
-			return json_encode(["isError" => true, "message" => "Invalid or unknown state"]);
+			return ["isError" => true, "message" => "Invalid or unknown state"];
 		} else if (strlen($_POST['FirstName']) == 0 || strlen($_POST['FirstName']) > 255) {
-			return json_encode(["isError" => true, "message" => "First Name must be between 1 and 255 characters"]);
+			return ["isError" => true, "message" => "First Name must be between 1 and 255 characters"];
 		} else if (strlen($_POST['LastName']) == 0 || strlen($_POST['LastName']) > 255) {
-			return json_encode(["isError" => true, "message" => "Last Name must be between 1 and 255 characters"]);
+			return ["isError" => true, "message" => "Last Name must be between 1 and 255 characters"];
 		}  else if (!strtotime($_POST['DOB'])) {
-			return json_encode(["isError" => true, "message" => "Invalid date of birth"]);
+			return ["isError" => true, "message" => "Invalid date of birth"];
 		} else if (!in_array($_POST['Gender'], $Gender)) {
-			return json_encode(["isError" => true, "message" => "Invalid or unknown pet gender"]);
+			return ["isError" => true, "message" => "Invalid or unknown pet gender"];
 		} else if (strlen($_POST['StreetAddress']) == 0 || strlen($_POST['StreetAddress']) > 255) {
-			return json_encode(["isError" => true, "message" => "Street Address must be between 1 and 255 characters"]);
+			return ["isError" => true, "message" => "Street Address must be between 1 and 255 characters"];
 		} else if (strlen($_POST['SuburbTown']) == 0 || strlen($_POST['SuburbTown']) > 255) {
-			return json_encode(["isError" => true, "message" => "Suburb/Town must be between 1 and 255 characters"]);
+			return ["isError" => true, "message" => "Suburb/Town must be between 1 and 255 characters"];
 		} else if (strlen($_POST['PostCode']) != 4 || filter_var(FILTER_SANITIZE_NUMBER_INT, $_POST['PostCode'])) {
-			return json_encode(["isError" => true, "message" => "Post Code must be 4 digits"]);
+			return ["isError" => true, "message" => "Post Code must be 4 digits"];
 		} else if (strlen($_POST['EmailAddress']) == 0 || strlen($_POST['EmailAddress']) > 255) {
-			return json_encode(["isError" => true, "message" => "Email Address must be between 1 and 255 characters"]);
+			return ["isError" => true, "message" => "Email Address must be between 1 and 255 characters"];
 		} else if (strlen($_POST['PhoneNumber']) != 10 || filter_var(FILTER_SANITIZE_NUMBER_INT, $_POST['PhoneNumber'])) {
-			return json_encode(["isError" => true, "message" => "Phone Number must 10 digits"]);
+			return ["isError" => true, "message" => "Phone Number must 10 digits"];
 		} else if (!$this->array_in_array($_POST['PetState'], $PetState)) {
-			return json_encode(["isError" => true, "message" => "Invalid or unknown pet state"]);
+			return ["isError" => true, "message" => "Invalid or unknown pet state"];
 		} else if ($_POST['PetState']['others'] && strlen($_POST['Comment']) == 0) {
-			return json_encode(["isError" => true, "message" => "Comment can't be empty"]);
+			return ["isError" => true, "message" => "Comment can't be empty"];
 		}
 		return ["isError" => false, "message" => "Validation success"];
 	}
