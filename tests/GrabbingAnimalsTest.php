1d9b7bb82a81593cdab8960db304304c68e01599
tommy hyland <102142713@student.swin.edu.au>
2019-05-29 01:44:01 +1000

    Added Grabbing Animals Testing

---
+++ tests/GrabbingAnimalsTest.php
@@ -0,0 +1,26 @@
+<?php
+require_once (__DIR__).'/../PHP Functions/includeAll.php';
+use PHPUnit\Framework\TestCase;
+class GrabbingAnimalsTest extends TestCase
+{
+  protected function setUp(): void
+	{
+    $_POST = array();
+    $_GET = array();
+    $_FILES = array();
+    $this->GrabbingAnimals = new GrabbingAnimals(true);
+  }
+  public function testNoData(): void
+  {
+    $output = $this->GrabbingAnimals->run();
+    $this->assertEquals(false, $output["isError"]);
+    $this->assertEquals("There is no data within the mysql database", $output["message"]);
+  }
+  public function testData(): void
+  {
+    $output = $this->GrabbingAnimals->run();
+    $this->assertEquals(true, $output["isError"]);
+    $this->assertEquals("Data is within the mysql database", $output["message"]);
+  }
+}
+?>
