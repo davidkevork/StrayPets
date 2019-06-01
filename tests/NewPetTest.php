8004738baf3d6fbfc32b09fb163207eac8b5b575
tommy hyland <102142713@student.swin.edu.au>
2019-06-01 20:08:19 +1000

    Creating unit tests for NewPetTest

---
+++ tests/NewPetTest.php
@@ -0,0 +1,48 @@
+<?php
+require_once (__DIR__).'/../PHP Functions/includeAll.php';
+
+use PHPUnit\Framework\TestCase;
+
+class NewPetTest extends TestCase
+{
+  protected function setUp(): void
+	{
+    $_POST = array();
+    $_GET = array();
+    $_FILES = array(
+			'test' => array(
+				'name' => 'poodletest.jpg',
+				'type' => 'image/jpg',
+				'size' => 4395,
+				'tmp_name' => 'poodletest.jpg',
+				'error' => 0
+			)
+	);
+
+    $this->NewPet = new NewPet(true, 'poodletest.jpg');
+  }
+  
+  protected function tearDown()
+  {
+	  unset($_FILES);
+	  unset(%this->NewPet);
+	  @unlink('poodletest.jpg');
+  }
+  
+  public function testReceive()
+  {
+	  $this->assertTrue($this->NewPet->receive('test'));
+  }
+  
+ // public function testCreatingDataNotWorking() : void 
+ // {
+//	  $_POST = [
+//		"Species" => "Dog",
+//		"Breed" => "Cocker-Spaniel",
+	//	"DOB" => "2019-06-01",
+//		"Gender" => "male",
+//		"PetName" => "Rufus",
+//		"
+ // }
+//}
+?>
